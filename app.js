02f1aba185041490c2f278bcd80e3cd72d98ef2e
ReturnNada <156492056+ReturnNada@users.noreply.github.com>
2024-03-21 17:47:58 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+const pi = 3.14159265;
+let username;
+
 function alertButton() {
-    alert("Hello World");
+    alert("Less clicking, more listening >:(");
+}
+
+document.getElementById("button1").onclick = function(){
+
+    username = document.getElementById("myText").value;
+    console.log(username);
+
+    let realname = window.prompt("Are you sure? Something seems fishy about that name... Enter another!");
+
+    document.getElementById("p1").innerHTML = "Hello " + realname + "!";
+    document.getElementById("p2").innerHTML = "Welcome to the glorious website...";
+    document.getElementById("p3").innerHTML = "So much to do!";
+}
+
+document.getElementById("button2").onclick = function(){
+
+    age = document.getElementById("myText2").value;
+    age = Number(age);
+    age++;
+
+    document.getElementById("p4").innerHTML = "Something tragic will happen when you are " + age + ", good luck!";
 }
