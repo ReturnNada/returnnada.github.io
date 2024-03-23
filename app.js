b3843078e3bcd35015b97c2c6d160376cf853ce9
ReturnNada <156492056+ReturnNada@users.noreply.github.com>
2024-03-23 17:22:51 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const pi = 3.14159265;
+const pi = Math.PI;
 let username;
+let realname = "";
 
 function alertButton() {
     alert("Less clicking, more listening >:(");
@@ -7,10 +8,12 @@ function alertButton() {
 
 document.getElementById("button1").onclick = function(){
 
+    //make something have to be there
     username = document.getElementById("myText").value;
-    console.log(username);
 
-    let realname = window.prompt("Are you sure? Something seems fishy about that name... Enter another!");
+    while(realname == null || realname.trim() == ""){
+        realname = window.prompt("Are you sure? Something seems fishy about that name... Enter another!");
+    }
 
     document.getElementById("p1").innerHTML = "Hello " + realname + "!";
     document.getElementById("p2").innerHTML = "Welcome to the glorious website...";
@@ -19,9 +22,38 @@ document.getElementById("button1").onclick = function(){
 
 document.getElementById("button2").onclick = function(){
 
+    //make something have to be there
     age = document.getElementById("myText2").value;
     age = Number(age);
     age++;
 
     document.getElementById("p4").innerHTML = "Something tragic will happen when you are " + age + ", good luck!";
+}
+
+document.getElementById("button3").onclick = function(){
+    if(document.getElementById("myCheckbox").checked){
+        window.alert("You subscribed!");
+    }
+    else{
+        window.alert("You didn't subscribe :(");
+    }
+}
+
+document.getElementById("button4").onclick = function(){
+    const visaBtn = document.getElementById("visaBtn");
+    const mastercardBtn = document.getElementById("mastercardBtn");
+    const paypalBtn = document.getElementById("paypalBtn");
+
+    if(visaBtn.checked){
+        window.alert("Visa used");
+    }
+    else if(mastercardBtn.checked){
+        window.alert("Mastercard used");
+    }
+    else if(paypalBtn.checked){
+        window.alert("Paypal used");
+    }
+    else{
+        window.alert("No money :(");
+    }
 }
