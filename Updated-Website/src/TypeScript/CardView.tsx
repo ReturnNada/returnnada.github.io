1026c6789870a58a762fc8dd305d1bd8221f3f26
Lucas Robson <returnnada@gmail.com>
2026-06-13 22:48:27 -0400

    Update CardView.tsx

--- Updated-Website/src/TypeScript/CardView.tsx
+++ Updated-Website/src/TypeScript/CardView.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import CASS8 from "../assets/CASS8.png";
 import Electrium from "../assets/Electrium.png";
 import Sketchee from "../assets/Sketchee.png";
 import Hitbox from "../assets/Hitbox.jpg";
